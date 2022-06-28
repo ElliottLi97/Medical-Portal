d9947badac94d77e67887c6afd8bfa7794814e66
Elliott <elliottli.job@gmail.com>
2022-06-27 20:57:05 -0700

    added models -index

---
+++ models/patients.js
@@ -0,0 +1,68 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Patient extends Model { }
+
+Patient.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        gender: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                isEmail: true
+            }
+        },
+        address:{
+            type: DataTypes.STRING
+        },
+        medical_id:{
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'history',
+              key: 'id',
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [8],
+            },
+          }
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            },
+          },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'patient',
+    }
+);
+
+module.exports = Patient;
+
+
