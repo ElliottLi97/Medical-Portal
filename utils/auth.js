a50497e2d8d81e6462c6ac03e60eb49d057c43e1
CharlesK <seung.kim1998@gmail.com>
2022-07-09 13:16:42 -0700

    added public folder for js and css?

--- utils/auth.js
+++ utils/auth.js
@@ -1,5 +1,4 @@
 const withAuth = (req, res, next) => {
-<<<<<<< HEAD
     if (!req.session.logged_in) {
       res.redirect('/login');
     } else {
@@ -9,14 +8,3 @@ const withAuth = (req, res, next) => {
   
   module.exports = withAuth;
   
-=======
-  // If the user is not logged in, redirect the request to the login route
-  if (!req.session.logged_in) {
-    res.redirect('/login');
-  } else {
-    next();
-  }
-};
-
-module.exports = withAuth;
->>>>>>> 91600e72c060cc9e8756c735e49b95b59b904b8a
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
     format_date: (date) => {
       // Format date as MM/DD/YYYY
       return date.toLocaleDateString();
@@ -21,26 +20,3 @@ module.exports = {
       }
     },
   };
-=======
-  format_date: (date) => {
-    // Format date as MM/DD/YYYY
-    return date.toLocaleDateString();
-  },
-  format_amount: (amount) => {
-    // format large numbers with commas
-    return parseInt(amount).toLocaleString();
-  },
-  get_emoji: () => {
-    const randomNum = Math.random();
-
-    // Return a random emoji
-    if (randomNum > 0.7) {
-      return `<span for="img" aria-label="lightbulb">💡</span>`;
-    } else if (randomNum > 0.4) {
-      return `<span for="img" aria-label="laptop">💻</span>`;
-    } else {
-      return `<span for="img" aria-label="gear">⚙️</span>`;
-    }
-  },
-};
->>>>>>> 91600e72c060cc9e8756c735e49b95b59b904b8a
